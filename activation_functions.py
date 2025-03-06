Fixing gradient

activation_functions.py
@@ -39,7 +39,8 @@ class Tanh(Activation):
         return "tanh"
 
     def forward(self, x):
-        self.output = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+        # self.output = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+        self.output = np.tanh(x)
         return self.output
 
     def backward(self, grad):

layers.py
@@ -28,9 +28,8 @@ class Linear(Layer):
         self.output_size = output_size
         self.activation = activation
 
-        self.weights = np.random.randn(self.input_size, self.output_size) * np.sqrt(
-            2 / (self.input_size + self.output_size)
-        )
+        self.weights = np.random.randn(self.input_size, self.output_size)
+        # * np.sqrt(    2 / (self.input_size + self.output_size))
         self.bias = np.zeros((1, output_size))
 
     def __repr__(self):
@@ -60,10 +59,10 @@ class Linear(Layer):
 
         # Step 1: Backprop through the activation function.
         # Compute the derivative of the activation function at the pre-activation values.
-        activation_derivative = self.activation.backward(self.z)
+        grad_pre_activation = self.activation.backward(output_grad)
 
-        # Multiply element-wise with the gradient coming from the next layer.
-        grad_pre_activation = output_grad * activation_derivative
+        # # Multiply element-wise with the gradient coming from the next layer.
+        # grad_pre_activation = output_grad * activation_derivative
 
         # Step 2: Compute gradients with respect to weights and biases.
         # The gradient for the weights is computed as the dot product of the transpose of the input

models.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     print(model)
 
     # Generate sine wave data
-    x = np.linspace(0, 1 * np.pi, 500).reshape(-1, 1)  # 10x more points
+    x = np.linspace(0, 2 * np.pi, 500).reshape(-1, 1)  # 10x more points
     y = np.sin(x) + np.random.normal(0, 0.01, x.shape)  # Add slight noise
 
     # Alternative: Use Min-Max Scaling (range [-1,1])
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     print(f"Number of samples: {len(x[0])}")
     print(f"Data shape: {x[0].shape}")
 
-    loss = model.fit(x_train, y_train, 100, 0.01)
+    loss = model.fit(x_train, y_train, 300, 0.001)
 
     plt.plot(loss)
     plt.show()